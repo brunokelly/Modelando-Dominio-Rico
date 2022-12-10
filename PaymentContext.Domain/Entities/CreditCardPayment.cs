c155d9563af1bb30329f08baf274bbb7886d4a7b
Bruno Kelly <45316003+brunokelly@users.noreply.github.com>
2022-12-10 18:08:31 -0300

    Corrupção no codigo

---
+++ PaymentContext.Domain/Entities/CreditCardPayment.cs
@@ -0,0 +1,34 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+    public class CreditCardPayment : Payment
+    {
+        public CreditCardPayment(string creditCardNumber, string cardHolderName, string lastTransactionNumber, string number,
+            DateTime paidDate,
+            DateTime expireDate,
+            decimal total,
+            decimal totalPaid,
+            string address,
+            string document,
+            string payer,
+            string email) : base(number,
+                paidDate,
+                expireDate,
+                total,
+                totalPaid,
+                address,
+                document,
+                payer,
+                email)
+        {
+            CreditCardNumber = creditCardNumber;
+            CardHolderName = cardHolderName;
+            LastTransactionNumber = lastTransactionNumber;
+        }
+
+        public string CreditCardNumber { get; private set; }
+        public string CardHolderName { get; private set; }
+        public string LastTransactionNumber { get; private set; }
+    }
+
+
+}
---
+++ PaymentContext.Domain/Entities/PaypalPayment.cs
@@ -0,0 +1,31 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+    public class PaypalPayment : Payment
+    {
+        public PaypalPayment(string transactionCode, 
+            string number,
+            DateTime paidDate,
+            DateTime expireDate,
+            decimal total,
+            decimal totalPaid,
+            string address,
+            string document,
+            string payer,
+            string email) : base(number,
+                paidDate,
+                expireDate,
+                total,
+                totalPaid,
+                address,
+                document,
+                payer,
+                email)
+        {
+            TransactionCode = transactionCode;
+        }
+
+        public string TransactionCode { get; private set; }
+    }
+
+
+}
